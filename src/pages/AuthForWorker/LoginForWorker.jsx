d491be135edc934ce4f70989bf03944218f98048
ViNguyenThanh <vint011103@gmail.com>
2025-03-19 01:04:55 +0700

    css and responsive for login and register for worker

---
+++ src/pages/AuthForWorker/LoginForWorker.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Auth from '../../components/AuthForWorker/AuthForWorker'
+
+const Login = () => {
+  return (
+    <div>
+        <Auth comp="Login"></Auth>
+    </div>
+  )
+}
+
+export default Login
