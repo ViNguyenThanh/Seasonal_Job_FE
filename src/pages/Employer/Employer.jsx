136ec5b8288242135f82fb6530bb010ae1cbbf68
ViNguyenThanh <vint011103@gmail.com>
2025-04-05 13:07:24 +0700

    add breadcrumb for employer job group detail

--- src/pages/Employer/Employer.jsx
+++ src/pages/Employer/Employer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './Employer.css'
-import Header from '../../components/Header/Header'
+import EmployerHeader from '../../components/EmployerHeader/EmployerHeader'
 import Footer from '../../components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 const Employer = () => {
   return (
     <div className='employer-whole-container'>
-      <Header />
+      <EmployerHeader />
       <div className="employer-container">
         <Outlet/>
       </div>
