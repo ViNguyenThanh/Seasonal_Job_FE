7e866c7da05c848099ac1db04c17e510c8c8a734
ViNguyenThanh <vint011103@gmail.com>
2025-04-09 16:29:20 +0700

    css, responsive and logic for employer application management page part application job groups, applications by job postings, application worker detail (not responsive yet)

---
+++ src/components/Employer/ApplicationJobGroups/ApplicationJobGroups.jsx
@@ -0,0 +1,164 @@
+import React, { useEffect, useState } from 'react'
+import './ApplicationJobGroups.css'
+import { ArrowRightOutlined, FolderOpenOutlined } from '@ant-design/icons';
+import { Empty, Input, Pagination, Select, Skeleton, Spin } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllJobGroupsByUserId } from '../../../redux/actions/jobgroups.action';
+const { Search } = Input;
+
+const ApplicationJobGroups = () => {
+  const jobGroups = [
+    { id: 1, title: 'Tổ Chức Và Giám Sát Công Tác Chuẩn Bị Cho Sự Kiện Triển Lãm Sản Phẩm Tiêu Dùng Tại Các Trung Tâm Thương Mại.', numberOfJobPostings: 5, status: 'inactive', startDate: '07/04/2025', endDate: '24/04/2025' },
+    { id: 2, title: 'Tổ Chức Và Giám Sát Công Tác Chuẩn Bị Cho Sự Kiện Triển Lãm Nghệ Thuật.', numberOfJobPostings: 7, status: 'active', startDate: '03/05/2025', endDate: '13/05/2025' },
+    { id: 3, title: 'Tổ Chức Các Chuyến Tham Quan Cho Khách Tham Dự Hội Nghị Quốc Tế Về Nghiên Cứu Môi Trường.', numberOfJobPostings: 8, status: 'inactive', startDate: '04/05/2025', endDate: '14/05/2025' },
+    { id: 4, title: 'Hỗ Trợ Việc Setup Âm Thanh, Ánh Sáng Và Thiết Bị Cho Các Sự Kiện Văn Hóa, Sự Kiện Lễ Hội.', numberOfJobPostings: 6, status: 'active', startDate: '05/05/2025', endDate: '15/05/2025' },
+    { id: 5, title: 'Quản Lý Các Công Việc Hậu Cần Cho Các Sự Kiện Thể Thao, Đảm Bảo An Ninh Và Vận Chuyển Hàng Hóa.', numberOfJobPostings: 1, status: 'completed', startDate: '06/05/2025', endDate: '16/05/2025' },
+    { id: 6, title: 'Giám Sát Công Tác Phục Vụ Các Bữa Ăn Và Sự Kiện Hội Nghị Cho Các Khách Tham Gia.', numberOfJobPostings: 2, status: 'inactive', startDate: '07/05/2025', endDate: '17/05/2025' },
+    { id: 7, title: 'Quản Lý Các Công Việc Hậu Cần Cho Các Sự Kiện Giáo Dục Và Đào Tạo Tại Trường Học.', numberOfJobPostings: 6, status: 'inactive', startDate: '08/05/2025', endDate: '18/05/2025' },
+    { id: 8, title: 'Cung Cấp Dịch Vụ Phục Vụ Tiệc Và Đảm Bảo Vệ Sinh Trong Các Sự Kiện Tiệc Cưới Và Tiệc Lớn.', numberOfJobPostings: 9, status: 'active', startDate: '09/05/2025', endDate: '19/05/2025' },
+    { id: 9, title: 'Phụ Trách Vận Hành Dịch Vụ Vận Chuyển Hành Lý Cho Các Sự Kiện Hội Nghị Quốc Tế Tại Các Khách Sạn.', numberOfJobPostings: 3, status: 'active', startDate: '10/05/2025', endDate: '20/05/2025' },
+    { id: 10, title: 'Tổ Chức Và Giám Sát Công Tác Chuẩn Bị Cho Sự Kiện Triển Lãm Sản Phẩm Tiêu Dùng Tại Các Trung Tâm Thương Mại.', numberOfJobPostings: 1, status: 'inactive', startDate: '11/05/2025', endDate: '21/05/2025' },
+    { id: 11, title: 'Cung Cấp Dịch Vụ Phục Vụ Tiệc Và Đảm Bảo Vệ Sinh Trong Các Sự Kiện Tiệc Cưới Và Tiệc Lớn.', numberOfJobPostings: 7, status: 'completed', startDate: '12/05/2025', endDate: '22/05/2025' },
+    { id: 12, title: 'Giám Sát Công Tác Phục Vụ Các Bữa Ăn Và Sự Kiện Hội Nghị Cho Các Khách Tham Gia.', numberOfJobPostings: 3, status: 'completed', startDate: '13/05/2025', endDate: '23/05/2025' },
+    { id: 13, title: 'Quản Lý Tài Liệu Và Giúp Đỡ Nhân Viên Trong Việc Phân Phối Thông Tin Về Sự Kiện Văn Hóa.', numberOfJobPostings: 6, status: 'completed', startDate: '14/05/2025', endDate: '24/05/2025' },
+    { id: 14, title: 'Chạy Và Giám Sát Các Công Tác Chuẩn Bị, Cài Đặt Thiết Bị Cho Sự Kiện Triển Lãm Nghệ Thuật.', numberOfJobPostings: 3, status: 'completed', startDate: '15/05/2025', endDate: '25/05/2025' },
+    { id: 15, title: 'Cung Cấp Dịch Vụ Phục Vụ Tiệc Và Đảm Bảo Vệ Sinh Trong Các Sự Kiện Tiệc Cưới Và Tiệc Lớn.', numberOfJobPostings: 10, status: 'completed', startDate: '16/05/2025', endDate: '26/05/2025' }
+  ];
+
+  const getStatusClass = (status) => {
+    if (status === 'active') return 'active';
+    if (status === 'completed') return 'completed';
+    if (status === 'inactive') return 'inactive';
+    return '';
+  };
+
+  // Quản lý phân trang
+  const [currentPage, setCurrentPage] = useState(1); // Trạng thái trang hiện tại
+  const pageSize = 12; // Mỗi trang hiển thị 12 dòng
+  const dispatch = useDispatch();
+  const { isLoading, error, payload } = useSelector(state => state.jobGroupsReducer);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    // window.scrollTo(0, 0);
+    window.scroll({ top: 100, left: 0, behavior: 'smooth' })
+  };
+
+  useEffect(() => {
+    dispatch(getAllJobGroupsByUserId());
+  }, [dispatch]);
+
+
+  // chức năng Search Job Posting
+  const [searchTerm, setSearchTerm] = useState('');
+  const [statusJobGroupValue, setStatusJobGroupValue] = useState(null);
+  const onSearch = (value) => {
+    setSearchTerm(value.trim().toLowerCase());
+    setStatusJobGroupValue(null);
+    setCurrentPage(1);     // Reset to the first page
+  };
+
+  const filteredJobGroups = /*jobGroups*/ !isLoading && payload?.length > 0 ? payload.filter(item => {
+    const searchTermLower = searchTerm.toLowerCase();
+    return (
+      (!statusJobGroupValue || item.status === statusJobGroupValue) &&
+      (!searchTerm
+        || item.title.toLowerCase().includes(searchTermLower)
+      )
+    );
+  }) : [];
+
+  // Phân trang dữ liệu (cắt dữ liệu theo trang)
+  const paginatedData = /*listData*/ filteredJobGroups.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+
+  const navigate = useNavigate()
+
+  return (
+    <div className='application-job-groups-container'>
+
+      {payload?.length === 0 ? (
+        <div className="no-job-groups">
+          <Empty description="You do not have a job group yet!" />;
+        </div>
+      ) : (
+        <>
+          <h1>Applications</h1>
+          <p className='application-job-groups-title'><FolderOpenOutlined /> Total number of <br /> Job Groups: <span>{payload?.length}</span></p>
+
+          <div className="application-job-groups-search">
+            <Search
+              placeholder="Search Job Group Name..."
+              value={searchTerm} // Giữ từ tìm kiếm
+              allowClear
+              enterButton
+              size="large"
+              onSearch={onSearch}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <Select
+              showSearch
+              placeholder="Status"
+              value={statusJobGroupValue}
+              onChange={(value) => {
+                setStatusJobGroupValue(value);
+                setCurrentPage(1);
+              }}
+              filterOption={(input, option) =>
+                (option?.label ?? '').toLowerCase().includes(input.toLowerCase())
+              }
+              size="large"
+              allowClear
+              options={[
+                { value: 'completed', label: 'Completed' },
+                { value: 'active', label: 'Active' },
+                { value: 'inactive', label: 'Inactive' },
+              ]}
+            />
+          </div>
+
+          {isLoading ? (
+            <div className="application-job-groups-list-skeleton">
+              <Skeleton paragraph={{ rows: 3 }} />
+              <Skeleton paragraph={{ rows: 3 }} />
+              <Skeleton paragraph={{ rows: 3 }} />
+            </div>
+          ) : filteredJobGroups.length === 0 ? (
+            <div className="no-job-groups">
+              <Empty description="No job groups found!" />;
+            </div>
+          ) : (
+            <>
+              <div className="application-job-groups-list">
+                {/*jobGroups*/ paginatedData.map((group) => (
+                  <div className="application-job-groups-item" key={group.id} onClick={() => navigate(`/employer/application/job-groups/${group.id}`, { state: group }, window.scrollTo(0, 0))}>
+                    <p className='job-group-name'>{group.title}</p>
+                    <p className='number-of-job-postings'>Number of Job Postings: {group.totalJobPostings}</p>
+                    <p className={`status ${getStatusClass(group.status)}`}>{group.status}</p>
+                    <div style={{ display: 'none' }}>
+                      {group.startDate}
+                      {group.endDate}
+                    </div>
+                    <button><ArrowRightOutlined /></button>
+                  </div>
+                ))}
+              </div>
+
+              <Pagination
+                current={currentPage}
+                pageSize={pageSize}
+                total={filteredJobGroups.length}
+                onChange={handlePageChange}
+                showSizeChanger={false}
+                align="center"
+              />
+            </>
+          )}
+
+        </>
+      )}
+    </div>
+  )
+}
+
+export default ApplicationJobGroups
---
+++ src/components/Employer/ApplicationWorkerDetail/ApplicationWorkerDetail.jsx
@@ -0,0 +1,226 @@
+import React, { useState } from 'react'
+import './ApplicationWorkerDetail.css'
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Breadcrumb, Button, message, Modal, Rate } from 'antd';
+import { ArrowLeftOutlined, ArrowRightOutlined, ContainerOutlined, EnvironmentOutlined, ExclamationCircleOutlined, EyeOutlined, FileDoneOutlined, FileTextOutlined, FolderOpenOutlined, GiftOutlined, IdcardOutlined, MailOutlined, PhoneOutlined, SnippetsOutlined, SolutionOutlined, UserSwitchOutlined } from '@ant-design/icons';
+import avatar from '/assets/Work-On-Computer.png'
+
+const ApplicationWorkerDetail = () => {
+
+  const navigate = useNavigate()
+  const location = useLocation();
+  const item = location.state // gồm thông tin của 1 jobGroup (jobGroupInfo) và thông tin của 1 worker (workerInfo)
+  // console.log(item)
+
+  /* Hiển thị file pdf */
+  const [previewVisible, setPreviewVisible] = useState(false); // hiển thị modal preview
+  // const [cvFile, setCvFile] = useState(null); // lưu trữ file CV
+  // Mô phỏng file CV mặc định
+  // const defaultCvFile = new File([""], "defaultCV.pdf", { type: "application/pdf" });
+  const defaultCvFileUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Freviewcongnghe.net%2Fhinh-nen-may-tinh-dep%2F&psig=AOvVaw30VWPW_bAo63KTJJAMvS9i&ust=1744266952200000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMim5vKqyowDFQAAAAAdAAAAABAE";
+
+
+  // Hiển thị modal khi nhấn vào 'Preview CV'
+  const showPreview = () => {
+    // setCvFile(defaultCvFile); // Đặt file CV mặc định
+    setPreviewVisible(true); // Hiển thị modal preview
+  };
+
+  // Đóng modal
+  const closePreview = () => {
+    setPreviewVisible(false);
+  };
+
+  /* Confirm Aprrove or Reject */
+  const [confirmVisible, setConfirmVisible] = useState(false); // hiển thị modal xác nhận
+  const [actionType, setActionType] = useState(""); // Lưu loại hành động (Approve hoặc Reject)
+  const [confirmLoading, setConfirmLoading] = useState(false); // Xử lý loading khi bấm Yes
+
+  const [isApproved, setIsApproved] = useState(false); // Đã phê duyệt
+  const [isRejected, setIsRejected] = useState(false); // Đã từ chối
+
+  // Mở modal xác nhận khi bấm Approve hoặc Reject
+  const showConfirm = (action) => {
+    setActionType(action); // Lưu loại hành động là Approve hoặc Reject
+    setConfirmVisible(true); // Hiển thị modal xác nhận
+  };
+
+  // Xử lý xác nhận hành động (Approve hoặc Reject)
+  const handleConfirm = () => {
+    // setConfirmVisible(false);
+    // if (actionType === "approve") {
+    //   alert("Job Application Approved");
+    // } else if (actionType === "reject") {
+    //   alert("Job Application Rejected");
+    // }
+    setConfirmLoading(true); // Bắt đầu loading
+    setTimeout(() => {
+      setConfirmVisible(false); // Đóng modal sau 2 giây
+      setConfirmLoading(false); // Dừng loading
+      if (actionType === "approve") {
+        setIsApproved(true);
+        message.success("The application has been successfully approved.");
+      } else if (actionType === "reject") {
+        setIsRejected(true);
+        message.success("The application has been successfully rejected.");
+      }
+    }, 2000);
+  };
+
+  // Đóng modal xác nhận
+  const closeConfirm = () => {
+    setConfirmVisible(false);
+  };
+
+  return (
+    <div className='application-worker-detail-whole-container'>
+      <Breadcrumb className='breadcrumb'
+        items={[
+          {
+            title: <Link
+              to="/employer/application/job-groups"
+              className='b-title-1'
+            >
+              <FolderOpenOutlined /> List Job Groups
+            </Link>,
+          },
+          {
+            title:
+              // ko xài Link vì khó truyền state
+              (
+                <div className='b-title-2 gray' onClick={() => navigate(`/employer/application/job-groups/${item.jobGroupInfo.id}`, { state: item.jobGroupInfo })}>
+                  <ContainerOutlined />  {item.jobGroupInfo?.title}
+                </div>
+              )
+          },
+          {
+            title: (
+              <div className='b-title-2'>
+                <SolutionOutlined /> {item.workerInfo?.workerName}
+              </div>
+            ),
+          },
+        ]}
+      />
+
+      <div className='application-worker-detail-container'>
+        <div className="application-worker-detail-left">
+
+          <div className="worker-identity">
+            <img src={avatar} />
+            <div className="worker-name-star">
+              <p>{item.workerInfo?.workerName}</p>
+              <Rate defaultValue={4} disabled />
+            </div>
+          </div>
+
+          <div className="preview-cv">
+            <Button onClick={showPreview}><EyeOutlined />Preview CV</Button>
+
+            <Modal
+              title={<p className='modal-title'>Preview CV</p>}
+              open={previewVisible}
+              onCancel={closePreview}
+              footer={null}
+              width="80%"
+            >
+              {/* {cvFile ? (
+                cvFile.type === "application/pdf" ? (
+                  <embed
+                    // src={URL.createObjectURL(cvFile)}
+                    src={defaultCvFileUrl}
+                    type="application/pdf"
+                    width="100%"
+                    height="500px"
+                  />
+                ) : (
+                  <img
+                    alt="Preview"
+                    // src={URL.createObjectURL(cvFile)}
+                    // src={defaultCvFileUrl}
+                    style={{ width: "100%" }}
+                  />
+                )
+              ) : (
+                <p>No CV available for preview.</p>
+              )} */}
+              <embed
+                src={defaultCvFileUrl} // Đưa URL trực tiếp vào đây
+                type="application/pdf"
+                width="100%"
+                height="500px"
+              />
+            </Modal>
+          </div>
+          <div className='worker-cover-letter'>
+            <p><FileDoneOutlined /> Cover Letter: </p>
+            <p>I am an energetic individual with experience in seasonal jobs such as sales,
+              customer service support, and gift wrapping during holidays. I have also worked
+              in production environments with high workloads and participated in event organization,
+              assisting with exhibitions and fairs. I am adaptable, work efficiently under pressure,
+              and quickly adjust to job demands.</p>
+          </div>
+        </div>
+
+        <div className="application-worker-detail-right">
+          <h2>Worker Information</h2>
+          <div className="worker-info">
+            <p><MailOutlined /> {item.workerInfo?.email}</p>
+            <p><UserSwitchOutlined /> Female  </p> {/* -- None -- */}
+          </div>
+          <div className="worker-info">
+            <p><EnvironmentOutlined /> Tỉnh Tây Ninh, Huyện Dương Minh Châu</p>
+            <p><PhoneOutlined rotate={90} /> 0123456789 </p>
+          </div>
+          <div className="worker-info">
+            <p><GiftOutlined /> 01/01/2000</p>
+          </div>
+          <div className='worker-description'>
+            <p> <IdcardOutlined /> About me: </p>
+            <p>I am an energetic individual with experience in seasonal jobs such as sales,
+              customer service support, and gift wrapping during holidays. I have also worked
+              in production environments with high workloads and participated in event organization,
+              assisting with exhibitions and fairs. I am adaptable, work efficiently under pressure,
+              and quickly adjust to job demands.</p>
+              {/* <p>-- None --</p> */}
+          </div>
+        </div>
+
+        <div className="approve-reject-btn" >
+          {!isApproved && !isRejected ? (
+            <>
+              <button className='approve-btn' onClick={() => showConfirm("approve")}>Approve</button>
+              <button className='reject-btn' onClick={() => showConfirm("reject")}>Reject</button>
+            </>
+          ) : (
+            <button className={`disabled-btn ${isApproved ? 'approved' : 'rejected'}`} disabled>
+              {isApproved ? 'Approved' : 'Rejected'}
+            </button>
+          )}
+
+          <Modal
+            title={<p className='confirm-title'> <ExclamationCircleOutlined /> Confirm</p>}
+            open={confirmVisible}
+            onCancel={closeConfirm}
+            footer={[
+              <Button key="no" onClick={closeConfirm} size='large'>No</Button>,
+              <Button key="yes" type="primary" size='large' onClick={handleConfirm} loading={confirmLoading}>
+                Yes
+              </Button>,
+            ]}
+          >
+            <p className='confirm-content'>
+              Are you sure you want to{' '}
+              <span className={`confirm-type ${actionType === 'approve' ? 'approved' : 'rejected'}`}>
+                {actionType === "approve" ? "APPROVE" : "REJECT"}
+              </span>
+              {' '}{item.workerInfo?.workerName}'s application for the {item.jobGroupInfo?.title}? - Tuyển Nhân Viên Hậu Cần
+            </p>
+          </Modal>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ApplicationWorkerDetail
